9a0f79442f40a44c03045761af4905199e4bfabd
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-03-27 21:33:31 -0500

    29:29

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
+import './helpers/dotenv'
+
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+import pino from 'pino'
 
-const app = express()
-
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send({ msg: 'Martin Rodriguez' })
+const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true
+    }
+  }
 })
 
-app.listen(port)
+// const logger = pino({ prettyPrint: { colorize: true } })
+const app = express()
+
+const port = parseInt(process.env.PORT, 10) || 3000
+
+app.use(morgan(process.env.MORGAN_LOG))
+app.use(cors({ origin: process.env.ORIGIN }))
+app.use(helmet())
+
+app.get('/', (req, res) => {
+  logger.info('Inside the root path')
+  const title = process.env.TITLE || 'Server'
+  res.send({ msg: title })
+})
+
+app.listen(port, ()=>logger.info('Application started at http://localhost${process.env.PORT}'),)
