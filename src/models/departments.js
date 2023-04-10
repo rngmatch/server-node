e34f6e3e7d64ca9507e5d9f05babea71c7a682e5
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-04-10 14:55:18 -0500

    done

--- src/models/departments.js
+++ src/models/departments.js
@@ -3,7 +3,13 @@ import db from '../helpers/db'
 export const getDepartments = async () => db.department.findMany()
 
 export const getDepartment = async (id) =>
-  db.department.findUnique({ where: { departmentId: id } })
+  // db.department.findUnique({ where: { departmentId: id } })
+  db.company.findUnique({
+    where: { companyID: id },
+    include: {
+      departments: true,
+    },
+  })
 
 export const addDepartment = async (name, companyId) =>
   db.department.create({ data: { name, companyId } })
