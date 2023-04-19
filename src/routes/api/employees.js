93da3186f85b27a85e9b730492052a83cc880e22
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-04-18 23:51:09 -0500

    faker working with 10

--- src/routes/api/employees.js
+++ src/routes/api/employees.js
@@ -11,7 +11,15 @@ import {
 const router = Router()
 
 router.get('/', async (req, res) => {
-  const employees = await getEmployees()
+  const size = Number(req.query.size) || 10
+  const page = Number(req.query.page) || 1
+  const skip = size * (page - 1)
+  const take = size
+  const { count, employees } = await getEmployees(skip, take)
+  res.set({
+    'X-Total-Count': count,
+    'X-Total-Pages': Math.ceil(count / size),
+  })
   res.send(employees)
 })
 
